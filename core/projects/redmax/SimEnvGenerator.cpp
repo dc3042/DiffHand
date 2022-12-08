d9a1eb4f49743c5b6b253cc855234020cfe65c7b
chodh0119 <62224270+chodh0119@users.noreply.github.com>
2022-12-08 16:15:15 -0500

    default 4

--- core/projects/redmax/SimEnvGenerator.cpp
+++ core/projects/redmax/SimEnvGenerator.cpp
@@ -610,7 +610,7 @@ Simulation* SimEnvGenerator::createDavidCustomDemo(std::string integrator) {
 
     // add spring force
     ForceSpring* forceS1 = new ForceSpring(sim, body0, body1, 0, 0, 1, 4);
-    ForceSpring* forceS2 = new ForceSpring(sim, body0, body1, 1, 1, 4);
+    ForceSpring* forceS2 = new ForceSpring(sim, body0, body1, 1, 1, 1, 4);
     robot->add_force(forceS1);
     robot->add_force(forceS2);
 
