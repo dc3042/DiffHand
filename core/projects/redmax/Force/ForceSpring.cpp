7ac113c30069323cc9da7f23101beb14ed9c0707
chodh0119 <62224270+chodh0119@users.noreply.github.com>
2022-12-09 18:07:58 -0500

    spring coeff

--- core/projects/redmax/Force/ForceSpring.cpp
+++ core/projects/redmax/Force/ForceSpring.cpp
@@ -115,9 +115,9 @@ void ForceSpring::computeForceWithDerivative(
     Matrix3 K = I - (_l/l) * (I - nn);
 
     Km.block(_cuboid1->_index[0], _cuboid1->_index[0], 6, 6) += 
-            _k * G1.transpose() * (Matrix<dtype, 3, 6>() << math::skew(R1.transpose() * K * (xw2 - p1)), -K).finished();
+            _k * G1.transpose() * (Matrix<dtype, 3, 6>() << coeff * math::skew(R1.transpose() * (xw2 - p1)) + l * R1.skew(xl1) * nn, -K).finished();
     Km.block(_cuboid2->_index[0], _cuboid2->_index[0], 6, 6) +=
-        _k * G2.transpose() * (Matrix<dtype, 3, 6>() << math::skew(R2.transpose() * K * (xw1 - p2)), -K).finished();
+        _k * G2.transpose() * (Matrix<dtype, 3, 6>() << coeff * math::skew(R1.transpose() * (xw2 - p1)) + l * R1.skew(xl1) * nn, -K).finished();
     Km.block(_cuboid1->_index[0], _cuboid2->_index[0], 6, 6) +=
         _k * G1.transpose() * (R1.transpose() * (R2 * (Matrix<dtype, 3, 6>() << K * math::skew(xl2), K).finished()));
     Km.block(_cuboid2->_index[0], _cuboid1->_index[0], 6, 6) += 
