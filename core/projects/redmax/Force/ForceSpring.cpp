f45124b7b7c2c63bf8ff1c4fba23861351377cc2
chodh0119 <62224270+chodh0119@users.noreply.github.com>
2022-12-12 17:36:47 -0500

    spring force body

--- core/projects/redmax/Force/ForceSpring.cpp
+++ core/projects/redmax/Force/ForceSpring.cpp
@@ -22,15 +22,15 @@ void ForceSpring::set_k(dtype k) {
 void ForceSpring::computeForce(VectorX& fm, VectorX& fr, bool verbose) {
     //std::cout << "Hello ComputeForce" << std::endl;
 
-    Matrix4 E1 = _cuboid1->_E_0i;
+    Matrix4 E1 = _body1->_E_0i;
     Matrix3 R1 = E1.topLeftCorner(3, 3);
     Vector3 p1 = E1.topRightCorner(3, 1);
-    Matrix4 E2 = _cuboid2->_E_0i;
+    Matrix4 E2 = _body2->_E_0i;
     Matrix3 R2 = E2.topLeftCorner(3, 3);
     Vector3 p2 = E2.topRightCorner(3, 1);
 
-    Vector3 xl1 = _cuboid1->get_contact_points()[_contact1];
-    Vector3 xl2 = _cuboid1->get_contact_points()[_contact2];
+    Vector3 xl1 = _body1->get_contact_points()[_contact1];
+    Vector3 xl2 = _body1->get_contact_points()[_contact2];
 
     Vector3 xw1 = R1 * xl1 + p1;
     Vector3 xw2 = R2 * xl2 + p2;
@@ -44,8 +44,8 @@ void ForceSpring::computeForce(VectorX& fm, VectorX& fr, bool verbose) {
     dtype f =  - _k * (l - _l);
 
 
-    fm.segment(_cuboid1->_index[0], 6) +=  f/l * G1.transpose() * (R1.transpose() * dx);
-    fm.segment(_cuboid2->_index[0], 6) -=  f/l * G2.transpose() * (R2.transpose() * dx);
+    fm.segment(_body1->_index[0], 6) +=  f/l * G1.transpose() * (R1.transpose() * dx);
+    fm.segment(_body2->_index[0], 6) -=  f/l * G2.transpose() * (R2.transpose() * dx);
 
     //std::cout << "xw1 " << xw1 << std::endl;
     //std::cout << "xw2 " << xw2 << std::endl;
@@ -60,15 +60,15 @@ void ForceSpring::computeForceWithDerivative(
     VectorX& fm, VectorX& fr, MatrixX& Km, MatrixX& Dm, MatrixX& Kr, MatrixX& Dr, bool verbose) {
     //std::cout << "Hello ComputeForceWithDerivative" << std::endl;
 
-    Matrix4 E1 = _cuboid1->_E_0i;
+    Matrix4 E1 = _body1->_E_0i;
     Matrix3 R1 = E1.topLeftCorner(3, 3);
     Vector3 p1 = E1.topRightCorner(3, 1);
-    Matrix4 E2 = _cuboid2->_E_0i;
+    Matrix4 E2 = _body2->_E_0i;
     Matrix3 R2 = E2.topLeftCorner(3, 3);
     Vector3 p2 = E2.topRightCorner(3, 1);
 
-    Vector3 xl1 = _cuboid1->get_contact_points()[_contact1];
-    Vector3 xl2 = _cuboid1->get_contact_points()[_contact2];
+    Vector3 xl1 = _body1->get_contact_points()[_contact1];
+    Vector3 xl2 = _body1->get_contact_points()[_contact2];
 
     Vector3 xw1 = R1 * xl1 + p1;
     Vector3 xw2 = R2 * xl2 + p2;
@@ -83,8 +83,8 @@ void ForceSpring::computeForceWithDerivative(
 
     dtype f =  - _k * (l - _l); // No dependence on l_dot
 
-    fm.segment(_cuboid1->_index[0], 6) +=  f/l * G1.transpose() * R1.transpose() * dx;
-    fm.segment(_cuboid2->_index[0], 6) -=  f/l * G2.transpose() * R2.transpose() * dx;
+    fm.segment(_body1->_index[0], 6) +=  f/l * G1.transpose() * R1.transpose() * dx;
+    fm.segment(_body2->_index[0], 6) -=  f/l * G2.transpose() * R2.transpose() * dx;
 
     std::cout << "xw1 " << xw1 << std::endl;
     std::cout << "xw2 " << xw2 << std::endl;
@@ -110,34 +110,34 @@ void ForceSpring::computeForceWithDerivative(
 
     MatrixX K1 = dfl_dq * f_vector.transpose();
 
-    Km.block(_cuboid1->_index[0], _cuboid1->_index[0], 6, 6) += K1.block(0, 0, 6, 6);
-    Km.block(_cuboid1->_index[0], _cuboid2->_index[0], 6, 6) += K1.block(0, 6, 6, 6);
-    Km.block(_cuboid2->_index[0], _cuboid1->_index[0], 6, 6) += K1.block(6, 0, 6, 6);
-    Km.block(_cuboid2->_index[0], _cuboid2->_index[0], 6, 6) += K1.block(6, 6, 6, 6);
+    Km.block(_body1->_index[0], _body1->_index[0], 6, 6) += K1.block(0, 0, 6, 6);
+    Km.block(_body1->_index[0], _body2->_index[0], 6, 6) += K1.block(0, 6, 6, 6);
+    Km.block(_body2->_index[0], _body1->_index[0], 6, 6) += K1.block(6, 0, 6, 6);
+    Km.block(_body2->_index[0], _body2->_index[0], 6, 6) += K1.block(6, 6, 6, 6);
 
     /**
     K2
     */
 
-    Km.block(_cuboid1->_index[0], _cuboid1->_index[0], 3,3) += f/l * math::skew(xl1) * math::skew(R1.transpose() * (p1 - xw2));
-    Km.block(_cuboid1->_index[0], _cuboid1->_index[3], 3,3) += f/l * math::skew(xl1);
-    Km.block(_cuboid1->_index[0], _cuboid2->_index[0], 3,3) += f/l * math::skew(xl1) * R1.transpose() * R2 * math::skew(xl2);
-    Km.block(_cuboid1->_index[0], _cuboid2->_index[3], 3,3) += f/l * -math::skew(xl1) * R1.transpose() * R2;
+    Km.block(_body1->_index[0], _body1->_index[0], 3,3) += f/l * math::skew(xl1) * math::skew(R1.transpose() * (p1 - xw2));
+    Km.block(_body1->_index[0], _body1->_index[3], 3,3) += f/l * math::skew(xl1);
+    Km.block(_body1->_index[0], _body2->_index[0], 3,3) += f/l * math::skew(xl1) * R1.transpose() * R2 * math::skew(xl2);
+    Km.block(_body1->_index[0], _body2->_index[3], 3,3) += f/l * -math::skew(xl1) * R1.transpose() * R2;
 
-    Km.block(_cuboid1->_index[3], _cuboid1->_index[0], 3,3) += f/l * math::skew(R1.transpose() * (p1 - xw2));
-    Km.block(_cuboid1->_index[3], _cuboid1->_index[3], 3,3) += f/l * I;
-    Km.block(_cuboid1->_index[3], _cuboid2->_index[0], 3,3) += f/l * R1.transpose() * R2 * math::skew(xl2);
-    Km.block(_cuboid1->_index[3], _cuboid2->_index[3], 3,3) += f/l * -R1.transpose() * R2;
+    Km.block(_body1->_index[3], _body1->_index[0], 3,3) += f/l * math::skew(R1.transpose() * (p1 - xw2));
+    Km.block(_body1->_index[3], _body1->_index[3], 3,3) += f/l * I;
+    Km.block(_body1->_index[3], _body2->_index[0], 3,3) += f/l * R1.transpose() * R2 * math::skew(xl2);
+    Km.block(_body1->_index[3], _body2->_index[3], 3,3) += f/l * -R1.transpose() * R2;
 
-    Km.block(_cuboid2->_index[0], _cuboid1->_index[0], 3,3) += f/l * math::skew(xl2) * R2.transpose() * R1 * math::skew(xl1);
-    Km.block(_cuboid2->_index[0], _cuboid1->_index[3], 3,3) += f/l * -math::skew(xl2) * R2.transpose() * R1;
-    Km.block(_cuboid2->_index[0], _cuboid2->_index[0], 3,3) += f/l * math::skew(xl2) * math::skew(R2.transpose() * (p2 - xw1));
-    Km.block(_cuboid2->_index[0], _cuboid2->_index[3], 3,3) += f/l * math::skew(xl2);
+    Km.block(_body2->_index[0], _body1->_index[0], 3,3) += f/l * math::skew(xl2) * R2.transpose() * R1 * math::skew(xl1);
+    Km.block(_body2->_index[0], _body1->_index[3], 3,3) += f/l * -math::skew(xl2) * R2.transpose() * R1;
+    Km.block(_body2->_index[0], _body2->_index[0], 3,3) += f/l * math::skew(xl2) * math::skew(R2.transpose() * (p2 - xw1));
+    Km.block(_body2->_index[0], _body2->_index[3], 3,3) += f/l * math::skew(xl2);
 
-    Km.block(_cuboid2->_index[3], _cuboid1->_index[0], 3,3) += f/l * R2.transpose() * R1 * math::skew(xl1);
-    Km.block(_cuboid2->_index[3], _cuboid1->_index[3], 3,3) += f/l * -R2.transpose() * R1;
-    Km.block(_cuboid2->_index[3], _cuboid2->_index[0], 3,3) += f/l * math::skew(R2.transpose() * (p2 - xw1));
-    Km.block(_cuboid2->_index[3], _cuboid2->_index[3], 3,3) += f/l * I;
+    Km.block(_body2->_index[3], _body1->_index[0], 3,3) += f/l * R2.transpose() * R1 * math::skew(xl1);
+    Km.block(_body2->_index[3], _body1->_index[3], 3,3) += f/l * -R2.transpose() * R1;
+    Km.block(_body2->_index[3], _body2->_index[0], 3,3) += f/l * math::skew(R2.transpose() * (p2 - xw1));
+    Km.block(_body2->_index[3], _body2->_index[3], 3,3) += f/l * I;
 
 }
 
