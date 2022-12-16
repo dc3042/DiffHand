faf58d5ef631d05f9221c3febe57676f4ab0354a
chodh0119 <62224270+chodh0119@users.noreply.github.com>
2022-12-16 17:23:12 -0500

    robot

--- examples/robot_walk/example_walk.py
+++ examples/robot_walk/example_walk.py
@@ -66,8 +66,8 @@ if __name__ == '__main__':
     q_init = sim.get_q_init().copy()
 
     print(q_init)
-    q_init[-1] = np.pi / 6.
-    q_init[1] = np.pi / 6.
+    q_init[-1] = -np.pi / 6.
+    q_init[1] = -np.pi / 6.
     sim.set_q_init(q_init)
 
     num_steps = 500
