f59973cce279bb6c26b2b1e48f646e8ebca2fc32
chodh0119 <62224270+chodh0119@users.noreply.github.com>
2022-12-19 10:33:23 -0500

    knuckle

--- examples/robot_walk/example_walk.py
+++ examples/robot_walk/example_walk.py
@@ -68,20 +68,20 @@ if __name__ == '__main__':
     #print(len(q_init))
     
     q_init[-1] = np.pi / 3.
-    q_init[-2] = -np.pi / 6.
-    q_init[-3] = np.pi / 2
+    q_init[-2] = -np.pi / 2.
+    q_init[-3] = 0
     
     q_init[-4] = np.pi / 3.
-    q_init[-5] = -np.pi / 6.
-    q_init[-6] = np.pi / 2.
+    q_init[-5] = -np.pi / 2.
+    q_init[-6] = 0
 
     q_init[-7] = np.pi / 3.
-    q_init[-8] = -np.pi / 6.
-    q_init[-9] = np.pi / 2.
+    q_init[-8] = -np.pi / 2.
+    q_init[-9] = 0
 
     q_init[-10] = np.pi / 3.
-    q_init[-11] = -np.pi / 6.
-    q_init[-12] = np.pi / 2.
+    q_init[-11] = -np.pi / 2.
+    q_init[-12] = 
     
     sim.set_q_init(q_init)
 
